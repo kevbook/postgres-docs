41946df40c493c8aec0f1bcaa2ffe7a3e3e24b92
Karan Sakhuja <karan.sakhuja@outlook.com>
2023-01-13 15:43:52 -0500

    Create performance.sql

---
+++ performance.sql
@@ -0,0 +1,22 @@
+# Performance
+
+Identifying poorly performing queries using `pg_stat_statements`
+
+```sqlSELECT
+  pg_roles.rolname AS "user",
+  "query",
+  -- Number of times the query has been called
+  calls AS "num_calls",
+  -- Total time spent executing the query
+  (ROUND(total_exec_time::numeric / 1000, 2) || 's') AS "total_time",
+  (ROUND(mean_exec_time::numeric / 1000, 2) || 's') AS "mean_time",
+  (ROUND(stddev_exec_time::numeric / 1000, 2) || 's') AS "stddev_time",
+  ROUND((100 * total_exec_time / SUM(total_exec_time::numeric) OVER())::numeric, 2) AS "percentage_overall"
+
+FROM extensions.pg_stat_statements
+  JOIN pg_roles ON (userid=oid)
+
+WHERE pg_roles.rolname !~ '^(pg_|rds)'
+ORDER BY mean_time DESC
+LIMIT 25;
+```
